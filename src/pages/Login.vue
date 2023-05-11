5aa0692b7f7960b642cb910411da120649f590a5
chrysarana <chrystian.arana@linktic.com>
2023-05-11 15:07:22 -0500

    fixes login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,8 +2,14 @@
 import { ref } from 'vue'
 import womenlogin from "assets/women-login.svg"
 import proLinkTicLogo from "assets/ProLinkTic.svg"
-import login from "assets/final-login.svg"
+import loginImg from "assets/final-login.svg"
 //import triggerError from './components/trigger-error.vue';
+import service from "../services/LoginService"
+
+const user = ref("")
+const pass = ref("")
+
+const isPwd = ref(false)
 
 const error = ref(false)
 
@@ -11,6 +17,13 @@ const error = ref(false)
 const errorHandler = (e: any) => {
   e.preventDefault()
   error.value = true
+}
+
+
+
+const loginHandler = (e) => {
+  e.preventDefault()
+  service.login(user.value, pass.value)
 }
 </script>
 
@@ -54,45 +67,31 @@ const errorHandler = (e: any) => {
             <div class="tw-py-6">
               <label for="username" class="tw-block tw-text-gray-inputs tw-text-lg tw-mx-2 tw-mb-1">Usuario</label>
               <div class="tw-relative">
-                <span class="tw-absolute tw-inset-y-0 tw-left-0 tw-pl-3 tw-w-7 tw-flex tw-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#969EAF">
-                    <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17
-                    71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2
-                    16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132
-                      0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0
-                      35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
-                  </svg>
-                </span>
-                <input type="text" id="username" name="username" placeholder="Ingrese su email" required
-                  class="tw-border tw-rounded-xl tw-py-3 tw-pl-10 tw-w-full tw-shadow-sm">
+
+                <q-input type="text" id="username" name="username" placeholder="Ingrese su email" required
+                  class=" tw-rounded-xl tw-py-3  tw-w-full tw-shadow-sm tw-border-gray-inputs" v-model="user" outlined>
+                  <template v-slot:prepend>
+                    <q-icon name="o_mail" />
+                  </template>
+                </q-input>
               </div>
             </div>
 
             <div>
               <label for="username" class="tw-block tw-text-gray-inputs tw-text-lg tw-mx-2 tw-mb-1">Contraseña</label>
               <div class="tw-relative">
-                <span class="tw-absolute tw-inset-y-0 tw-left-0 tw-pl-3 tw-flex tw-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#969EAF" class="tw-bi tw-bi-lock"
-                    viewBox="0 0 16 16">
-                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2
-                      0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0
-                       1-1-1V9a1 1 0 0 1 1-1z" />
-                  </svg>
-                </span>
-                <input type="password" id="password" name="password" placeholder="Ingrese su Ingrese su clave" required
-                  class="tw-border tw-rounded-xl tw-py-3 tw-pl-10 tw-w-full tw-shadow-sm">
-                <span class="tw-absolute tw-inset-y-0 tw-right-0 tw-pr-3 tw-w-8 tw-flex tw-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#969EAF"
-                    class="tw-bi tw-bi-eye tw- tw-hidden sm:tw-block tw-" tw-viewBox="0 tw-0 tw-16 tw-16">
-                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133
-                        13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168
-                         2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83
-                          1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134
-                           13.134 0 0 1 1.172 8z" />
-                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5
-                            8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                  </svg>
-                </span>
+                <q-input v-model="pass" :type="isPwd ? 'password' : 'text'" hint="Password with toggle" id="password"
+                  name="password" placeholder="Ingrese su Ingrese su clave" required
+                  class=" tw-rounded-xl tw-py-3 tw-w-full tw-shadow-sm" outlined>
+                  <template v-slot:prepend>
+                    <q-icon name="o_lock" />
+                  </template>
+                  <template v-slot:append>
+                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+                      @click="isPwd = !isPwd" />
+                  </template>
+                </q-input>
+
               </div>
             </div>
 
@@ -108,7 +107,7 @@ const errorHandler = (e: any) => {
                 </label>
               </div>
 
-              <button type="submit" @click="errorHandler"
+              <button type="submit" @click="loginHandler"
                 class="tw-flex tw-justify-center tw-items-center tw-w-auto tw-bg-blue-linktic tw-text-white tw-py-3 tw-rounded-lg tw-font-semibold tw-mt-5">Ingresar</button>
               <a href="#" class="tw-font-bold tw-text-blue-intense tw-mx-auto tw-my-8">Registrarme</a>
             </div>
@@ -124,7 +123,7 @@ const errorHandler = (e: any) => {
 
       <!--IMAGEN HOJAS-->
       <div class="tw-relative tw-mt-auto">
-        <img :src="login" alt="final-login" class="tw-w-1/2 tw-absolute tw-bottom-0 tw-right-0" style="z-index: tw--1;">
+        <img :src="loginImg" alt="final-login" class="tw-w-1/2 tw-absolute tw-bottom-0 tw-right-0">
       </div>
 
     </div>
